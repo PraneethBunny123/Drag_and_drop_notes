6566dfa31ccee35c6c3086e8cb27fd3fc9247aeb
praneeth <gp.gunturu@gmail.com>
2025-05-25 12:54:19 -0500

    determining the random position inside the screen to render the notes.

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,6 +1,32 @@
+import { useEffect } from "react"
 import Note from "./Note"
 
 export default function Notes({notes, setNotes}) {
+
+    useEffect(() => {
+        const savedNotes = []
+
+        const updatedNotes = notes.map(note => {
+            const savedNotes = null
+            if(savedNotes) {
+                return {}
+            }else {
+                const position = determineNewPosition()
+                return {...note, position}
+            }
+        })
+    }, [])
+
+    function determineNewPosition() {
+        const maxX = window.innerWidth - 250
+        const maxY = window.innerHeight - 250
+
+        return {
+            x: Math.floor(Math.random() * maxX),
+            y: Math.floor(Math.random() * maxY)
+        }
+    }
+
     return (
         <div>
             {notes.map(note => (
