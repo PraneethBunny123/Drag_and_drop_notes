f355c3d8ac20e337d8f40b4c08e0661f1afa812f
praneeth <gp.gunturu@gmail.com>
2025-05-25 12:44:23 -0500

    created styles for the note cards

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,7 +1,20 @@
-export default function Note({content}) {
+export default function Note({content, ...props}) {
     return (
-        <div>
-            
+        <div
+            style={{
+                position: "absolute",
+                left: 0,
+                right: 0,
+                border: "1px solid black",
+                userSelect: "none",
+                padding: '10px',
+                width: "200px",
+                cursor: "move",
+                backgroundColor: "lightyellow"
+            }}
+            {...props}
+        >
+            📌{content}
         </div>
     )
 }
